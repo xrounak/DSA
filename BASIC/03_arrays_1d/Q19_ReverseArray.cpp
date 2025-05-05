6349ab3cfe42f8e03e7263becd3474192e4b16be
xrounak <rounakkumar13005@gmail.com>
2025-05-05 21:56:05 +0530

    Array complete

--- BASIC/03_arrays_1d/Q19_ReverseArray.cpp
+++ BASIC/03_arrays_1d/Q19_ReverseArray.cpp
@@ -11,6 +11,7 @@ void printarray(int arr[], int n){
     cout << endl;
 }
 
+//This function stores reversed array in a new array.
 void revrse(int arr[], int n){
     int revArr[n];
 
@@ -21,11 +22,22 @@ void revrse(int arr[], int n){
 
 }
 
+
+// This function revrses the existing array.
+void revrse2(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        arr[i] = arr[i]+arr[n-1-i];
+        arr[n-1-i] = arr[i] - arr[n-1-i];
+        arr[i] = arr[i]-arr[n-1-i];
+    }
+}
+
 int main() {
     int n=7;
     int array[n] = {45,55,23,54,54,21,2};
     printarray(array,n);
-    revrse(array,n);
+    revrse2(array,n);
+    printarray(array,n);
     
     return 0;
 }
